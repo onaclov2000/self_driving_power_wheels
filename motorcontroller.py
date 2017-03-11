38137b37458d7d09807b03a6ac24bc95dea0e530
Tyson Bailey <onaclov2000+github@gmail.com>
2017-03-11 08:36:47 -0700

    Update motorcontroller.py

--- motorcontroller.py
+++ motorcontroller.py
@@ -19,27 +19,27 @@ class MotorController:
       self.servo_max = max  # Max pulse length out of 4096
 
       # Set frequency to 60hz, good for servos. Good for power wheel?
-      pwm.set_pwm_freq(60)
+      self.pwm.set_pwm_freq(60)
       self.zeroize()
-      pwm.set_pwm(self.channels["INH_1"], 0, 4095)
-      pwm.set_pwm(self.channels["INH_2"], 0, 4095)
+      self.pwm.set_pwm(self.channels["INH_1"], 0, 4095)
+      self.pwm.set_pwm(self.channels["INH_2"], 0, 4095)
 
    def zeroize(self):
       for k in self.channels:
-         pwm.set_pwm(self.channels[k],0,0)
+         self.pwm.set_pwm(self.channels[k],0,0)
          
    def forward(self, time):
       try:
-         pwm.set_pwm(self.channels["IN_2"], 0, 0)
+         self.pwm.set_pwm(self.channels["IN_2"], 0, 0)
          if True:
-            pwm.set_pwm(self.channels["IN_1"], 0, self.servo_max)
+            self.pwm.set_pwm(self.channels["IN_1"], 0, self.servo_max)
       except:
           self.zeroize()
    def reverse(self, time):
       try:
-         pwm.set_pwm(self.channels["IN_1"], 0, 0)
+         self.pwm.set_pwm(self.channels["IN_1"], 0, 0)
          if True:
-            pwm.set_pwm(self.channels["IN_2"], 0, self.servo_max)
+            self.pwm.set_pwm(self.channels["IN_2"], 0, self.servo_max)
       except:
           self.zeroize()
       

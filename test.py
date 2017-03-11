9cdb8c7029d2b73a208ccc1574b91115b8e765a9
Tyson Bailey <onaclov2000+github@gmail.com>
2017-03-11 15:45:20 +0000

    Working example of both wheels being controlled

--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import sys
 right = MotorController(channels={'IN_1' : 0, 'IN_2' : 1, 'INH_1' : 2, 'INH_2' : 3})
 left =  MotorController(channels={'IN_1' : 4, 'IN_2' : 5, 'INH_1' : 7, 'INH_2' : 6})
 right.reverse(10)
-#left.reverse(10)
+#right.reverse(10)
+right.zeroize()
+left.forward(10)
+left.zeroize()
 #while True:
 #   try:
 #      pass
 #   except:
-#left.zeroize()
-right.zeroize()
+#      left.zeroize()
+#      right.zeroize()
 #      break
       
